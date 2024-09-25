d5133b6b6c278fff6a4a7f67564c6d40b22956f5
Matthew Oduamafu <matthew.oduamafu@turntabl.io>
2024-09-25 11:15:40 +0000

    minor

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 import javax.sql.DataSource;
 
@@ -48,11 +46,11 @@ public class DemoSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer -> {
-            configurer.requestMatchers( HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                    .requestMatchers( HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                    .requestMatchers( HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                    .requestMatchers( HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                    .requestMatchers( HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+            configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
                     .anyRequest().authenticated();
         });
 
@@ -62,6 +60,13 @@ public class DemoSecurityConfig {
         // disable csrf (cross site request forgery)
         // in general, not required for stateless REST APIs that use POST, PUT, GET, DELETE and/or PATCH
         http.csrf().disable();
+
+
+        // Ensure headers are set to prevent common vulnerabilities
+        http.headers().contentSecurityPolicy("script-src 'self'").and()
+                .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER).and()
+                .permissionsPolicy(policy -> policy.policy("geolocation 'none'; microphone 'none'; camera 'none'")).and()
+                .frameOptions().deny();
 
         return http.build();
     }
